1be09022f0cd723f05f9ac0733354ffbe7ab2e64
JonathanMary <jon85ma@gmail.com>
2021-01-25 23:12:48 +0100

    MVP Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { Route } from "react-router-dom";
+
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +18,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +35,14 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route path={"/movies/:id"} component="Movie" >
+          <Movie />
+        </Route>
+        <Route exact path={"/"} component="MovieList">
+          <MovieList movies={movieList}/>
+        </Route>
+      </div>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function MovieList(props) {
+
+  //console.log("MovieList/MovieList: ", props);
+  
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -11,10 +15,13 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+
+  //console.log("MovieList/MovieDetails: ", props);
+  const history = useHistory();
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={event => history.push(`/movies/${id}`)} >
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
